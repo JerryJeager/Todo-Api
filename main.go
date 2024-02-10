8a3682460a12f4b86b0b1083734bcc0a588313ca
Amadi Jerry <amadijerry310@gmail.com>
2024-02-10 01:04:17 +0100

    feat: created get endpoint for all todos

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	// "net/http"
 )
+
+type todo struct {
+	ID        string `json:"id"`
+	NAME      string `json:"name"`
+	COMPLETED bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", NAME: "Wash dirty clothes", COMPLETED: false},
+	{ID: "2", NAME: "Watch season 5 of My Hero Acamedia", COMPLETED: false},
+	{ID: "3", NAME: "Learn Backend Development with Go", COMPLETED: false},
+	{ID: "4", NAME: "Buy Apple Vision Pro", COMPLETED: false},
+	{ID: "5", NAME: "Watch season 2 of Jujutsu Kaisen", COMPLETED: true},
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
 
 func main() {
 	router := gin.Default()
+	router.GET("/todos", getTodos)
 
 	router.Run("localhost:8080")
 
